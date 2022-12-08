290957d3b14123ae13b1b9d87d015fd21e397e43
kabbasi98 <54915171+kabbasi98@users.noreply.github.com>
2022-12-07 17:03:08 -0800

    Add files via upload

---
+++ CSC656project2/dgemv-vectorized.cpp
@@ -0,0 +1,34 @@
+const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
+
+/*
+ * This routine performs a dgemv operation
+ * Y :=  A * X + Y
+ * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
+ * On exit, A and X maintain their input values.
+ */
+void my_dgemv(int n, double* A, double* x, double* y) {
+   // insert your code here: implementation of vectorized vector-matrix multiply
+
+   for(int j = 0; j <= n; j++){
+
+      A[j] = 0.0;
+
+      for(int i = 0; i <= n; i++){
+
+         A[j] += A[i] * x[i] + y[i];
+      }
+    
+   }
+
+   for (int row=0; row < n; row++){
+
+     int rowOffset = row*n;
+
+   for (int col=0; col < n; col++) {
+      
+      y[row] += A[col+rowOffset] * x[col];
+
+   }
+}
+
+}
